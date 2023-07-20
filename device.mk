181cfe140f86af3af727a0811bafd4997c929711
SamarV-121 <samarvispute121@pm.me>
2023-07-20 14:52:21 +0100

    vicky: Link older libutils for PQ HAL
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    Change-Id: I7ea86d3d7c68ba183b4838cb1f35a9fc58cfbf67

--- device.mk
+++ device.mk
@@ -76,7 +76,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1.vendor:64 \
     android.hardware.memtrack@1.0-service \
     android.hardware.memtrack@1.0-impl \
-    libdrm.vendor
+    libdrm.vendor \
+    libutils-v32
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
