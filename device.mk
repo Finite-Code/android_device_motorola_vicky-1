50dc32f723c62a8be059a29ae4cb06dea32d11fb
Sushrut1101 <guptasushrut@gmail.com>
2024-05-11 15:16:26 +0530

    vicky: Configure graphics stack

--- device.mk
+++ device.mk
@@ -34,6 +34,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Graphics
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.composer@2.1-resources.vendor:64 \
+    android.hardware.graphics.composer@2.1.vendor:64 \
+    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack@1.0-impl \
+    libdrm.vendor
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
